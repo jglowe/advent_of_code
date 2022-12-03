c58b0518f37f34142236ee822ce25159f5158b3c
Jonathan Lowe <me@jonathanlowe.dev>
2022-12-03 05:31:06 -0500

    Fixed up day 2

--- 2022/src/day2.rs
+++ 2022/src/day2.rs
@@ -12,7 +12,7 @@
 // Jonathan Lowe
 // github : https://github.com/jglowe
 //
-// The file for day1 advent of code 2022
+// The file for day2 advent of code 2022
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 use std::fs;
@@ -35,147 +35,145 @@ struct Round {
 }
 
 fn parse_rounds(input: String) -> Vec<Round> {
-    let mut rounds = Vec::new();
+    input
+        .trim()
+        .split("\n")
+        .map(|line| {
+            let choices: Vec<&str> = line.split(" ").collect();
 
-    for line in input.trim().split("\n") {
-        let choices: Vec<&str> = line.split(" ").collect();
+            assert!(
+                choices.len() == 2,
+                "There should only be two hands in a match"
+            );
 
-        assert!(
-            choices.len() == 2,
-            "There should only be two hands in a match"
-        );
+            let opponent_choice = match choices[0] {
+                "A" => Hand::Rock,
+                "B" => Hand::Paper,
+                    "C" => Hand::Scissors,
+                _ => panic!("Opponent didn't choose rock, paper, or scissors"),
+            };
 
-        let opponent_choice = match choices[0] {
-            "A" => Hand::Rock,
-            "B" => Hand::Paper,
-            "C" => Hand::Scissors,
-            _ => panic!("Opponent didn't choose rock, paper, or scissors"),
-        };
+            let your_choice = match choices[1] {
+                "X" => Choice::X,
+                "Y" => Choice::Y,
+                "Z" => Choice::Z,
+                _ => panic!("You didn't choose rock, paper, or scissors"),
+            };
 
-        let your_choice = match choices[1] {
-            "X" => Choice::X,
-            "Y" => Choice::Y,
-            "Z" => Choice::Z,
-            _ => panic!("You didn't choose rock, paper, or scissors"),
-        };
-
-        rounds.push(Round {
-            opponent: opponent_choice,
-            you: your_choice,
-        });
-    }
-
-    rounds
+            Round {
+                opponent: opponent_choice,
+                you: your_choice,
+            }
+        })
+        .collect()
 }
 
 fn compute_score(rounds: &Vec<Round>) -> i64 {
-    let mut score = 0;
+    rounds
+        .into_iter()
+        .map(|round| {
+            let choice_score = match &round.you {
+                Choice::X => 1,
+                Choice::Y => 2,
+                Choice::Z => 3,
+            };
 
-    for round in rounds {
-        let choice_score = match &round.you {
-            Choice::X => 1,
-            Choice::Y => 2,
-            Choice::Z => 3,
-        };
+            let win_score = match &round {
+                Round {
+                    opponent: Hand::Rock,
+                    you: Choice::X,
+                } => 3,
+                Round {
+                    opponent: Hand::Rock,
+                    you: Choice::Y,
+                } => 6,
+                Round {
+                    opponent: Hand::Rock,
+                    you: Choice::Z,
+                } => 0,
+                Round {
+                    opponent: Hand::Paper,
+                    you: Choice::X,
+                } => 0,
+                Round {
+                    opponent: Hand::Paper,
+                    you: Choice::Y,
+                } => 3,
+                Round {
+                    opponent: Hand::Paper,
+                    you: Choice::Z,
+                } => 6,
+                Round {
+                    opponent: Hand::Scissors,
+                    you: Choice::X,
+                } => 6,
+                Round {
+                    opponent: Hand::Scissors,
+                    you: Choice::Y,
+                } => 0,
+                Round {
+                    opponent: Hand::Scissors,
+                    you: Choice::Z,
+                } => 3,
+            };
 
-        let win_score = match &round {
-            Round {
-                opponent: Hand::Rock,
-                you: Choice::X,
-            } => 3,
-            Round {
-                opponent: Hand::Rock,
-                you: Choice::Y,
-            } => 6,
-            Round {
-                opponent: Hand::Rock,
-                you: Choice::Z,
-            } => 0,
-            Round {
-                opponent: Hand::Paper,
-                you: Choice::X,
-            } => 0,
-            Round {
-                opponent: Hand::Paper,
-                you: Choice::Y,
-            } => 3,
-            Round {
-                opponent: Hand::Paper,
-                you: Choice::Z,
-            } => 6,
-            Round {
-                opponent: Hand::Scissors,
-                you: Choice::X,
-            } => 6,
-            Round {
-                opponent: Hand::Scissors,
-                you: Choice::Y,
-            } => 0,
-            Round {
-                opponent: Hand::Scissors,
-                you: Choice::Z,
-            } => 3,
-        };
-
-        score = score + choice_score + win_score;
-    }
-
-    score
+            choice_score + win_score
+        })
+        .sum()
 }
 
 fn compute_score2(rounds: &Vec<Round>) -> i64 {
-    let mut score = 0;
+    rounds
+        .into_iter()
+        .map(|round| {
+            let choice_score = match &round {
+                Round {
+                    opponent: Hand::Rock,
+                    you: Choice::X,
+                } => 3,
+                Round {
+                    opponent: Hand::Rock,
+                    you: Choice::Y,
+                } => 1,
+                Round {
+                    opponent: Hand::Rock,
+                    you: Choice::Z,
+                } => 2,
+                Round {
+                    opponent: Hand::Paper,
+                    you: Choice::X,
+                } => 1,
+                Round {
+                    opponent: Hand::Paper,
+                    you: Choice::Y,
+                } => 2,
+                Round {
+                    opponent: Hand::Paper,
+                    you: Choice::Z,
+                } => 3,
+                Round {
+                    opponent: Hand::Scissors,
+                    you: Choice::X,
+                } => 2,
+                Round {
+                    opponent: Hand::Scissors,
+                    you: Choice::Y,
+                } => 3,
+                Round {
+                    opponent: Hand::Scissors,
+                    you: Choice::Z,
+                } => 1,
+            };
 
-    for round in rounds {
-        let choice_score = match &round {
-            Round {
-                opponent: Hand::Rock,
-                you: Choice::X,
-            } => 3,
-            Round {
-                opponent: Hand::Rock,
-                you: Choice::Y,
-            } => 1,
-            Round {
-                opponent: Hand::Rock,
-                you: Choice::Z,
-            } => 2,
-            Round {
-                opponent: Hand::Paper,
-                you: Choice::X,
-            } => 1,
-            Round {
-                opponent: Hand::Paper,
-                you: Choice::Y,
-            } => 2,
-            Round {
-                opponent: Hand::Paper,
-                you: Choice::Z,
-            } => 3,
-            Round {
-                opponent: Hand::Scissors,
-                you: Choice::X,
-            } => 2,
-            Round {
-                opponent: Hand::Scissors,
-                you: Choice::Y,
-            } => 3,
-            Round {
-                opponent: Hand::Scissors,
-                you: Choice::Z,
-            } => 1,
-        };
+            let win_score = match &round.you {
+                Choice::X => 0,
+                Choice::Y => 3,
+                Choice::Z => 6,
+            };
 
-        let win_score = match &round.you {
-            Choice::X => 0,
-            Choice::Y => 3,
-            Choice::Z => 6,
-        };
-
-        score = score + choice_score + win_score;
-    }
-
-    score
+            choice_score + win_score
+        })
+        .sum()
 }
 
 fn main() {
